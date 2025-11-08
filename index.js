cea586d93ce04050b00bcab132c2e011bcf0ca24
tanim206 <mdtasnimhossaintanim206@gmail.com>
2025-11-08 14:24:01 +0600

    8-11-25

--- index.js
+++ index.js
@@ -17,21 +17,48 @@
 //   };
 // };
 
-const getData = async () => {
-  const response = await fetch("https://dummyjson.com/products");
-  const storeData = await response.json();
-  console.log(storeData);
-  console.log("Heloo");
+// const getData = async () => {
+//   const response = await fetch("https://dummyjson.com/products");
+//   const storeData = await response.json();
+//   console.log(storeData);
+//   console.log("Heloo");
 
-  storeData?.products?.map((product) => {
-    document.getElementById("details").innerHTML += `
-    <div>
-     <p>${product?.id}</p>
-        <p>${product?.title}</p>
-        <p>${product?.description}</p>
-        <img src=${product?.thumbnail} />
-    </div>
-       
-        `;
-  });
+//   storeData?.products?.map((product) => {
+//     document.getElementById("details").innerHTML += `
+//     <div>
+//      <p>${product?.id}</p>
+//         <p>${product?.title}</p>
+//         <p>${product?.description}</p>
+//         <img src=${product?.thumbnail} />
+//     </div>
+
+//         `;
+//   });
+// };
+
+// Date 8-11-2025
+
+const getCardContainerEl = document.getElementById("card-container");
+
+const getProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  showProductOnUi(data)
 };
+
+
+const showProductOnUi=(data)=>{
+  data.products.map((product)=>{
+    const div = document.createElement("div")
+    div.classList.add("border-2","border-blue-500","rounded-md","p-5")
+    div.innerHTML += `
+      <img src=${product?.thumbnail}>
+      <p>${product?.title}</p>
+      <p>${product?.price}</p>
+      <button class="bg-blue-600 cursor-pointer text-white px-1 py-1 rounded-md my-5 text-center">Buy Now</button>
+      ` 
+    getCardContainerEl.appendChild(div)
+  })
+}
+
+
